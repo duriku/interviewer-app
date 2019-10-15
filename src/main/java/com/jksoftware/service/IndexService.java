23ad33ca1af5ae7ea2a44024f71e47c9c84cc6a1
duriku <kucera.duri@gmail.com>
2019-10-15 22:19:43 +0100

    grouping play

--- src/main/java/com/jksoftware/service/IndexService.java
+++ src/main/java/com/jksoftware/service/IndexService.java
@@ -1,8 +1,18 @@
 package com.jksoftware.service;
 
+import static com.jksoftware.config.InterviewerConfig.LAMBDA_INDEX_FOLDER;
 import com.jksoftware.converter.QuestionConverter;
+import static com.jksoftware.converter.QuestionConverter.questionToDocument;
 import com.jksoftware.dto.QuestionDTO;
 import com.jksoftware.model.Question;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import static java.util.Objects.nonNull;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -14,155 +24,140 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.TotalHitCountCollector;
 import org.apache.lucene.store.FSDirectory;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static com.jksoftware.config.InterviewerConfig.LAMBDA_INDEX_FOLDER;
-import static com.jksoftware.converter.QuestionConverter.questionToDocument;
-import static java.util.Objects.nonNull;
-
 public class IndexService {
-    private static final Logger LOG = Logger.getLogger(IndexService.class);
-    private static final IndexService instance = new IndexService();
+	private static final Logger LOG = Logger.getLogger(IndexService.class);
+	private static final IndexService instance = new IndexService();
 
-    private StandardAnalyzer analyzer;
-    private FSDirectory index;
-    private IndexWriter writer;
+	private StandardAnalyzer analyzer;
+	private FSDirectory index;
+	private IndexWriter writer;
 
-    private IndexService() {
-        this.analyzer = new StandardAnalyzer();
-        try {
-            this.index = FSDirectory.open(Paths.get(LAMBDA_INDEX_FOLDER));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private IndexService() {
+		this.analyzer = new StandardAnalyzer();
+		try {
+			this.index = FSDirectory.open(Paths.get(LAMBDA_INDEX_FOLDER));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public static IndexService getInstance() {
-        return instance;
-    }
+	public static IndexService getInstance() {
+		return instance;
+	}
 
-    public List<QuestionDTO> searchQuestions(final String search, final List<String> tags) throws Exception {
-        final Query query = parseQuery(search, tags);
-        // final Query allDocsQuery = new MatchAllDocsQuery(); RETRIEVE ALL DOCS FOR TAGCLOUD!
+	public List<QuestionDTO> searchQuestions(final String search, final List<String> tags) throws Exception {
+		final Query query = parseQuery(search, tags);
 
-        // search the query
-        int hitsPerPage = 10;
-        final IndexReader reader = DirectoryReader.open(index);
-        final IndexSearcher searcher = new IndexSearcher(reader);
+		// search the query
+		int hitsPerPage = 10;
+		final IndexReader reader = DirectoryReader.open(index);
+		final IndexSearcher searcher = new IndexSearcher(reader);
 
 //        final TotalHitCountCollector countCollector = new TotalHitCountCollector();
 //        searcher.search(query, countCollector);
 //        searcher.collectionStatistics("tags");
-        final TopDocs docs = searcher.search(query, hitsPerPage);
-        return Stream.of(docs.scoreDocs)
-                .map(getScoreDocDocumentFunction(searcher))
-                .filter(Objects::nonNull)
-                .map(QuestionConverter::documentToQuestionDTO)
-                .collect(Collectors.toList());
-    }
+		final TopDocs docs = searcher.search(query, hitsPerPage);
+		return Stream.of(docs.scoreDocs)
+				.map(getScoreDocDocumentFunction(searcher))
+				.filter(Objects::nonNull)
+				.map(QuestionConverter::documentToQuestionDTO)
+				.collect(Collectors.toList());
+	}
 
 
-    public void indexQuestion(final Question question) throws IOException {
-        this.writer = new IndexWriter(index, new IndexWriterConfig(analyzer));
-        addQuestion(question, writer);
-        writer.close();
-    }
+	public void indexQuestion(final Question question) throws IOException {
+		this.writer = new IndexWriter(index, new IndexWriterConfig(analyzer));
+		addQuestion(question, writer);
+		writer.close();
+	}
 
-    // NOTE: use when recreate the index from mongo!
-    public void reindexAllQuestions(final List<Question> questions) throws IOException {
-        final IndexWriterConfig config = new IndexWriterConfig(analyzer);
-        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-        this.writer = new IndexWriter(index, config);
+	// NOTE: use when recreate the index from mongo!
+	public void reindexAllQuestions(final List<Question> questions) throws IOException {
+		final IndexWriterConfig config = new IndexWriterConfig(analyzer);
+		config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+		this.writer = new IndexWriter(index, config);
 
-        questions.forEach(question -> {
-            LOG.info("indexing " + question);
-            this.addQuestion(question, writer);
-        });
-        writer.close();
-    }
+		questions.forEach(question -> {
+			LOG.info("indexing " + question);
+			this.addQuestion(question, writer);
+		});
+		writer.close();
+	}
 
-    public void updateIndex(final Question question) throws IOException {
-        this.writer = new IndexWriter(index, new IndexWriterConfig(analyzer));
-        updateQuestion(question, writer);
-        writer.close();
-    }
+	public void updateIndex(final Question question) throws IOException {
+		this.writer = new IndexWriter(index, new IndexWriterConfig(analyzer));
+		updateQuestion(question, writer);
+		writer.close();
+	}
 
-    public void removeQuestionFromIndex(final String questionId) throws IOException {
-        this.writer = new IndexWriter(index, new IndexWriterConfig(analyzer));
-        deleteQuestion(questionId, writer);
-        writer.close();
-    }
+	public void removeQuestionFromIndex(final String questionId) throws IOException {
+		this.writer = new IndexWriter(index, new IndexWriterConfig(analyzer));
+		deleteQuestion(questionId, writer);
+		writer.close();
+	}
 
-    private BooleanQuery parseQuery(final String title, final List<String> tags) {
-        final BooleanQuery.Builder mainQueryBuilder = new BooleanQuery.Builder();
-        if (nonNull(title)) {
-            final Query titleQuery = new TermQuery(new Term("title", title));
-            mainQueryBuilder.add(titleQuery, BooleanClause.Occur.MUST);
-        }
+	private BooleanQuery parseQuery(final String title, final List<String> tags) {
+		final BooleanQuery.Builder mainQueryBuilder = new BooleanQuery.Builder();
+		if (nonNull(title)) {
+			final Query titleQuery = new TermQuery(new Term("title", title));
+			mainQueryBuilder.add(titleQuery, BooleanClause.Occur.MUST);
+		}
 
-        if (nonNull(tags)) {
-            final BooleanQuery.Builder tagsQueryBuilder = new BooleanQuery.Builder();
-            tags.forEach(tag -> {
-                final Query tagQuery = new TermQuery(new Term("tags", tag));
-                tagsQueryBuilder.add(tagQuery, BooleanClause.Occur.SHOULD);
-            });
-            mainQueryBuilder.add(tagsQueryBuilder.build(), BooleanClause.Occur.MUST);
-        }
+		if (nonNull(tags)) {
+			final BooleanQuery.Builder tagsQueryBuilder = new BooleanQuery.Builder();
+			tags.forEach(tag -> {
+				final Query tagQuery = new TermQuery(new Term("tags", tag));
+				tagsQueryBuilder.add(tagQuery, BooleanClause.Occur.SHOULD);
+			});
+			mainQueryBuilder.add(tagsQueryBuilder.build(), BooleanClause.Occur.MUST);
+		}
 
-        return mainQueryBuilder.build();
-    }
+		return mainQueryBuilder.build();
+	}
 
-    private void addQuestion(final Question question, final IndexWriter writer) {
-        final Document doc = questionToDocument(question);
-        try {
-            writer.addDocument(doc);
-        } catch (IOException e) {
-            throw new RuntimeException("Could not add doc", e);
-        }
-    }
+	private void addQuestion(final Question question, final IndexWriter writer) {
+		final Document doc = questionToDocument(question);
+		try {
+			writer.addDocument(doc);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not add doc", e);
+		}
+	}
 
-    private void updateQuestion(final Question question, final IndexWriter writer) {
-        final Document doc = questionToDocument(question);
-        try {
-            // NOTE update basically deletes + adds the updated document
-            writer.deleteDocuments(new Term("_id", question.getId().toHexString()));
-            writer.addDocument(doc);
-        } catch (IOException e) {
-            throw new RuntimeException("Could not add doc", e);
-        }
-    }
+	private void updateQuestion(final Question question, final IndexWriter writer) {
+		final Document doc = questionToDocument(question);
+		try {
+			// NOTE update basically deletes + adds the updated document
+			writer.deleteDocuments(new Term("_id", question.getId().toHexString()));
+			writer.addDocument(doc);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not add doc", e);
+		}
+	}
 
-    private void deleteQuestion(final String questionId, final IndexWriter writer) {
-        try {
-            writer.deleteDocuments(new Term("_id", questionId));
-        } catch (IOException e) {
-            throw new RuntimeException("Could not add doc", e);
-        }
-    }
+	private void deleteQuestion(final String questionId, final IndexWriter writer) {
+		try {
+			writer.deleteDocuments(new Term("_id", questionId));
+		} catch (IOException e) {
+			throw new RuntimeException("Could not add doc", e);
+		}
+	}
 
-    private Function<ScoreDoc, Document> getScoreDocDocumentFunction(final IndexSearcher searcher) {
-        return e -> {
-            try {
-                return searcher.doc(e.doc);
-            } catch (IOException ex) {
-                LOG.error("Could not search doc from the index!", ex);
-                return null;
-            }
-        };
-    }
+	private Function<ScoreDoc, Document> getScoreDocDocumentFunction(final IndexSearcher searcher) {
+		return e -> {
+			try {
+				return searcher.doc(e.doc);
+			} catch (IOException ex) {
+				LOG.error("Could not search doc from the index!", ex);
+				return null;
+			}
+		};
+	}
 
 }
--- src/main/java/org/apache/lucene/demo/facet/MultiCategoryListsFacetsExample.java
+++ src/main/java/org/apache/lucene/demo/facet/MultiCategoryListsFacetsExample.java
@@ -23,9 +23,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.*;
 import org.apache.lucene.facet.*;
 import org.apache.lucene.facet.taxonomy.FastTaxonomyFacetCounts;
 import org.apache.lucene.facet.taxonomy.TaxonomyReader;
@@ -36,127 +34,194 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
+import org.apache.lucene.search.grouping.GroupingSearch;
+import org.apache.lucene.search.grouping.TopGroups;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.util.BytesRef;
 
-/** Demonstrates indexing categories into different indexed fields. */
+/**
+ * Demonstrates indexing categories into different indexed fields.
+ */
 public class MultiCategoryListsFacetsExample {
 
-  private final Directory indexDir = new ByteBuffersDirectory();
-  private final Directory taxoDir = new ByteBuffersDirectory();
-  private final FacetsConfig config = new FacetsConfig();
+	private final Directory indexDir = new ByteBuffersDirectory();
+	private final Directory taxoDir = new ByteBuffersDirectory();
+	private final FacetsConfig config = new FacetsConfig();
 
-  /** Creates a new instance and populates the category list params mapping. */
-  public MultiCategoryListsFacetsExample() {
-    config.setIndexFieldName("Author", "author");
-    config.setIndexFieldName("Publish Date", "pubdate");
-    config.setHierarchical("Publish Date", true);
-  }
+	/**
+	 * Creates a new instance and populates the category list params mapping.
+	 */
+	public MultiCategoryListsFacetsExample() {
+		config.setIndexFieldName("Author", "author");
+		config.setIndexFieldName("Publish Date", "pubdate");
+		config.setHierarchical("Publish Date", true);
+	}
 
-  /** Build the example index. */
-  private void index() throws IOException {
-    IndexWriter indexWriter = new IndexWriter(indexDir, new IndexWriterConfig(
-        new WhitespaceAnalyzer()).setOpenMode(OpenMode.CREATE));
+	/**
+	 * Build the example index.
+	 */
+	private void index() throws IOException {
+		IndexWriter indexWriter = new IndexWriter(indexDir, new IndexWriterConfig(
+				new WhitespaceAnalyzer()).setOpenMode(OpenMode.CREATE));
 
-    // Writes facet ords to a separate directory from the main index
-    DirectoryTaxonomyWriter taxoWriter = new DirectoryTaxonomyWriter(taxoDir);
+		// Writes facet ords to a separate directory from the main index
+		DirectoryTaxonomyWriter taxoWriter = new DirectoryTaxonomyWriter(taxoDir);
 
-    Document doc = new Document();
-    doc.add(new FacetField("Author", "Bob"));
-    doc.add(new FacetField("Publish Date", "2010", "10", "15"));
-    doc.add(new TextField("title", "kutya", Field.Store.YES));
-    indexWriter.addDocument(config.build(taxoWriter, doc));
+		Document doc = new Document();
+		doc.add(new FacetField("Author", "Bob"));
+		doc.add(new FacetField("Publish Date", "2010", "10", "15"));
+		doc.add(new TextField("title", "kutya", Field.Store.YES));
+        addGroupField(doc, "category", "one");
+		indexWriter.addDocument(config.build(taxoWriter, doc));
 
-    doc = new Document();
-    doc.add(new FacetField("Author", "Lisa"));
-    doc.add(new FacetField("Publish Date", "2010", "10", "20"));
-    doc.add(new TextField("title", "kutya", Field.Store.YES));
-    indexWriter.addDocument(config.build(taxoWriter, doc));
+		doc = new Document();
+		doc.add(new FacetField("Author", "Lisa"));
+		doc.add(new FacetField("Publish Date", "2010", "10", "20"));
+		doc.add(new TextField("title", "kutya", Field.Store.YES));
+        addGroupField(doc, "category", "one");
+		indexWriter.addDocument(config.build(taxoWriter, doc));
 
-    doc = new Document();
-    doc.add(new FacetField("Author", "Lisa"));
-    doc.add(new FacetField("Publish Date", "2012", "1", "1"));
-    doc.add(new TextField("title", "kutya", Field.Store.YES));
-    indexWriter.addDocument(config.build(taxoWriter, doc));
+		doc = new Document();
+		doc.add(new FacetField("Author", "Lisa"));
+		doc.add(new FacetField("Publish Date", "2012", "1", "1"));
+		doc.add(new TextField("title", "kutya", Field.Store.YES));
+        addGroupField(doc, "category", "one");
+		indexWriter.addDocument(config.build(taxoWriter, doc));
 
-    doc = new Document();
-    doc.add(new FacetField("Author", "Susan"));
-    doc.add(new FacetField("Publish Date", "2012", "1", "7"));
-    doc.add(new TextField("title", "kutya", Field.Store.YES));
-    indexWriter.addDocument(config.build(taxoWriter, doc));
+		doc = new Document();
+		doc.add(new FacetField("Author", "Susan"));
+		doc.add(new FacetField("Publish Date", "2012", "1", "7"));
+		doc.add(new TextField("title", "kutya", Field.Store.YES));
+        addGroupField(doc, "category", "two");
+		indexWriter.addDocument(config.build(taxoWriter, doc));
 
-    doc = new Document();
-    doc.add(new FacetField("Author", "Frank"));
-    doc.add(new FacetField("Publish Date", "1999", "5", "5"));
-    doc.add(new TextField("title", "eger", Field.Store.YES));
-    indexWriter.addDocument(config.build(taxoWriter, doc));
+		doc = new Document();
+		doc.add(new FacetField("Author", "Frank"));
+		doc.add(new FacetField("Publish Date", "1999", "5", "5"));
+		doc.add(new TextField("title", "eger", Field.Store.YES));
+        addGroupField(doc, "category", "three");
+		indexWriter.addDocument(config.build(taxoWriter, doc));
 
-    indexWriter.close();
-    taxoWriter.close();
-  }
+		indexWriter.close();
+		taxoWriter.close();
+	}
 
-  /** User runs a query and counts facets. */
-  private List<FacetResult> search() throws IOException {
-    DirectoryReader indexReader = DirectoryReader.open(indexDir);
-    IndexSearcher searcher = new IndexSearcher(indexReader);
-    TaxonomyReader taxoReader = new DirectoryTaxonomyReader(taxoDir);
+	/**
+	 * User runs a query and counts facets.
+	 */
+	private List<FacetResult> search() throws IOException {
+		DirectoryReader indexReader = DirectoryReader.open(indexDir);
+		IndexSearcher searcher = new IndexSearcher(indexReader);
+		TaxonomyReader taxoReader = new DirectoryTaxonomyReader(taxoDir);
 
-    FacetsCollector fc = new FacetsCollector();
-
-    // MatchAllDocsQuery is for "browsing" (counts facets
-    // for all non-deleted docs in the index); normally
-    // you'd use a "normal" query:
-
-    // TODO: CHECK OUT GROUPING
-    // https://github.com/smartan/lucene/blob/f2e782a30fa8b466fab0926903e90f79a4177a3e/src/test/java/org/apache/lucene/search/grouping/TestGrouping.java
+		FacetsCollector fc = new FacetsCollector();
 
 
-    // TODO: Pagination
-    final Query titleQuery = new TermQuery(new Term("title", "kutya"));
-    final TopDocs topDocs = FacetsCollector.search(searcher, titleQuery, 1, fc);
-    System.out.println(topDocs.totalHits);
-    Stream.of(topDocs.scoreDocs).map(e -> {
-      try {
-        return searcher.doc(e.doc);
-      } catch (IOException ex) {
-        ex.printStackTrace();
-      }
-      return null;
-    }).filter(Objects::nonNull).forEach(System.out::println);
-//    FacetsCollector.search(searcher, new MatchAllDocsQuery(), 10, fc);
+//    paginationQuery(searcher, fc);
+//    normalQuery(searcher, fc);
+//    allQuery(searcher, fc);
+		groupQuery(searcher, fc);
 
-    // Retrieve results
-    List<FacetResult> results = new ArrayList<>();
 
-    // Count both "Publish Date" and "Author" dimensions
-    Facets author = new FastTaxonomyFacetCounts("author", taxoReader, config, fc);
-    results.add(author.getTopChildren(10, "Author"));
+		// Retrieve results
+		List<FacetResult> results = new ArrayList<>();
 
-    Facets pubDate = new FastTaxonomyFacetCounts("pubdate", taxoReader, config, fc);
-    results.add(pubDate.getTopChildren(10, "Publish Date"));
+		// Count both "Publish Date" and "Author" dimensions
+		Facets author = new FastTaxonomyFacetCounts("author", taxoReader, config, fc);
+		results.add(author.getTopChildren(10, "Author"));
 
-    indexReader.close();
-    taxoReader.close();
+		Facets pubDate = new FastTaxonomyFacetCounts("pubdate", taxoReader, config, fc);
+		results.add(pubDate.getTopChildren(10, "Publish Date"));
 
-    return results;
-  }
+		indexReader.close();
+		taxoReader.close();
 
-  /** Runs the search example. */
-  public List<FacetResult> runSearch() throws IOException {
-    index();
-    return search();
-  }
+		return results;
+	}
 
-  /** Runs the search example and prints the results. */
-  public static void main(String[] args) throws Exception {
-    System.out.println("Facet counting over multiple category lists example:");
-    System.out.println("-----------------------");
-    List<FacetResult> results = new MultiCategoryListsFacetsExample().runSearch();
-    System.out.println("Author: " + results.get(0));
-    System.out.println("Publish Date: " + results.get(1));
-  }
+	private void paginationQuery(final IndexSearcher searcher, final FacetsCollector fc) {
+		// TODO IMPL
+	}
+
+	private TopDocs allQuery(final IndexSearcher searcher, final FacetsCollector fc) throws IOException {
+		return FacetsCollector.search(searcher, new MatchAllDocsQuery(), 10, fc);
+	}
+
+	private void normalQuery(final IndexSearcher searcher, final FacetsCollector fc) throws IOException {
+		final Query titleQuery = new TermQuery(new Term("title", "kutya"));
+		final TopDocs topDocs = FacetsCollector.search(searcher, titleQuery, 1, fc);
+		System.out.println(topDocs.totalHits);
+		Stream.of(topDocs.scoreDocs).map(e -> {
+			try {
+				return searcher.doc(e.doc);
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			return null;
+		}).filter(Objects::nonNull).forEach(System.out::println);
+	}
+
+	private void groupQuery(final IndexSearcher searcher, final FacetsCollector fc) throws IOException {
+		// https://github.com/smartan/lucene/blob/f2e782a30fa8b466fab0926903e90f79a4177a3e/src/test/java/org/apache/lucene/search/grouping/TestGrouping.java
+		Sort groupSort = Sort.RELEVANCE;
+		final Query titleQuery = new TermQuery(new Term("title", "kutya"));
+
+
+		final GroupingSearch groupingSearch = createRandomGroupingSearch("category", groupSort, 10);
+		final TopGroups<Object> groups = groupingSearch.search(searcher, new MatchAllDocsQuery(), 0, 10);
+		System.out.println("groups count " + groups.totalGroupCount);
+
+
+		final TopDocs topDocs = FacetsCollector.search(searcher, titleQuery, 1, fc);
+		System.out.println(topDocs.totalHits);
+		Stream.of(topDocs.scoreDocs).map(e -> {
+			try {
+				return searcher.doc(e.doc);
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			return null;
+		}).filter(Objects::nonNull).forEach(System.out::println);
+	}
+
+	/**
+	 * Runs the search example.
+	 */
+	public List<FacetResult> runSearch() throws IOException {
+		index();
+		return search();
+	}
+
+	/**
+	 * Runs the search example and prints the results.
+	 */
+	public static void main(String[] args) throws Exception {
+		System.out.println("Facet counting over multiple category lists example:");
+		System.out.println("-----------------------");
+		List<FacetResult> results = new MultiCategoryListsFacetsExample().runSearch();
+		System.out.println("Author: " + results.get(0));
+		System.out.println("Publish Date: " + results.get(1));
+	}
+
+
+	private GroupingSearch createRandomGroupingSearch(String groupField, Sort groupSort, int docsInGroup) {
+		GroupingSearch groupingSearch;
+//		ValueSource vs = new BytesRefFieldSource(groupField);
+//		groupingSearch = new GroupingSearch(vs, new HashMap<>());
+		groupingSearch = new GroupingSearch(groupField);
+
+		groupingSearch.setGroupSort(groupSort);
+		groupingSearch.setGroupDocsLimit(docsInGroup);
+
+		groupingSearch.setCachingInMB(4.0, true);
+		return groupingSearch;
+	}
+
+	// https://github.com/apache/lucene-solr/blob/793635eb0ed429b7e38f999fd9c4dc2d131530e0/lucene/luke/src/test/org/apache/lucene/luke/models/documents/DocumentsTestBase.java
+	private void addGroupField(Document doc, String groupField, String value) {
+		doc.add(new TextField(groupField, value, Field.Store.YES));
+		doc.add(new SortedDocValuesField(groupField, new BytesRef(value)));
+	}
 }
